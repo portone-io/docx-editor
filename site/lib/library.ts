3851b09c56e9a9c94ebcdc13e7d9227b432eb9cc
Deea <gohyuk528@gmail.com>
2026-08-25 15:58:58 +0900

    feat(site): verify the site with Google Search Console (#27)

--- site/lib/library.ts
+++ site/lib/library.ts
@@ -5,4 +5,6 @@ export const libraryDescription = manifest.description;
 export const libraryVersion = manifest.version;
 export const repositoryUrl = "https://github.com/portone-io/docx-editor";
 export const siteUrl = manifest.homepage;
+export const googleSiteVerification =
+  "2hn5FQAHbj-6fwc6W85KxkCtnx2CbPPDAqzfq38TIxQ";
 export const docsRoute = "/docs";
